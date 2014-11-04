39f4c8a93db042b47db5d0f6774649b186149f79
Martin Rusev <martinrusev@zoho.com>
2014-11-04 13:01:50 +0200

    Homepage packages icons

--- _site/js/main.js
+++ _site/js/main.js
@@ -2,19 +2,38 @@
 $(function ()
 {	
 
+	_.templateSettings = {
+  		interpolate: /\{\{(.+?)\}\}/g
+	};
+
 	var qtip_options = {
 		content: {
-        		attr: 'data-tooltip' // Tell qTip2 to look inside this attr for its content
-    	},
+				attr: 'data-tooltip' // Tell qTip2 to look inside this attr for its content
+		},
 		style: { classes: 'qtip-bootstrap'},
 		 position: {
-		 	my: 'bottom center',
+			my: 'bottom center',
 			at: 'top center'
 		}
 	}
 
 	
 	$(".with-tooltip").qtip(qtip_options);
+
+	$.get("https://api.github.com/repos/martinrusev/edgium/contents/packages", function(data) {
+		var packages_list = $('.packages-list');
+
+		_.each(data, function(element){
+
+			var template = _.template("<li> {{ name }}</li>");
+			var compiled = template({name: element.name});	
+			
+			//packages_list.append(compiled)
+		});	
+
+
+    	
+	});
 
 
 });
